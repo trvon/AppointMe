9c8d580530d2b2576a8fe895a44d24d07a22925e
salad7 <msalad@uncc.edu>
2017-04-01 02:56:51 -0400

    Completed home fragment need to add firebase

---
+++ app/src/main/java/com/example/mohamed/timely4app/AvailablePros.java
@@ -0,0 +1,8 @@
+package com.example.mohamed.timely4app;
+
+/**
+ * Created by mohamed on 4/1/17.
+ */
+
+public class AvailablePros {
+}
--- app/src/main/java/com/example/mohamed/timely4app/HomeFragment.java
+++ app/src/main/java/com/example/mohamed/timely4app/HomeFragment.java
@@ -1,10 +1,45 @@
 package com.example.mohamed.timely4app;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by mohamed on 4/1/17.
  */
 
 public class HomeFragment extends Fragment{
+
+    EditText comp;
+    EditText id;
+    Button request;
+    Button signup;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.login_signup_fragment,container,false);
+        comp = (EditText) v.findViewById(R.id.companyNameET);
+        id= (EditText) v.findViewById(R.id.idET);
+        request = (Button) v.findViewById(R.id.request);
+        request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Send request for comp child for the persons id
+            }
+        });
+        signup = (Button) v.findViewById(R.id.signup);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Go to register fragment
+            }
+        });
+        return v;
+    }
 }
